7dde90e7d02e296cf07bcfe66f55bd61e512be62
dmytronayda <dima.nayda.1993@gmail.com>
2020-02-24 06:59:15 +0100

    Alternative solution to exercise 7.

--- arrays/exercise7.js
+++ arrays/exercise7.js
@@ -1,8 +1,8 @@
 let numbers = [3, 5, 7];
 
-let sumOfSquares = numbers.map(num => num * num)
-                          .reduce((acc, val) => acc + val, 0); 
+// let sumOfSquares = numbers.map(num => num * num)
+//                           .reduce((acc, val) => acc + val, 0); 
 
-
+let sumOfSquares = numbers.reduce((acc, val) => acc + val * val, 0); 
 
 console.log(sumOfSquares); // => 83
